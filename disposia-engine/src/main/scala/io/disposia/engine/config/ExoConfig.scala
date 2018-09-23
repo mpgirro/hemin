3d0253dc07039997bcd4b7af0b15411219991211
Maximilian Irro <max@irro.at>
2018-09-23 13:01:37 +0200

    new approach to provide default configs

--- disposia-engine/src/main/scala/io/disposia/engine/config/ExoConfig.scala
+++ disposia-engine/src/main/scala/io/disposia/engine/config/ExoConfig.scala
@@ -3,10 +3,11 @@ package io.disposia.engine.config
 import akka.util.Timeout
 import com.typesafe.config.{Config, ConfigFactory}
 
+import scala.collection.JavaConverters._
 import scala.concurrent.duration._
 
 case class ExoConfig (
-    appConfig: AppConfig,
+    appConfig: AppConfig,           // Config when we start an engine stand-alone
     catalogConfig: CatalogConfig,
     crawlerConfig: CrawlerConfig,
     indexConfig: IndexConfig,
@@ -16,40 +17,60 @@ case class ExoConfig (
 )
 
 object ExoConfig {
-    /** Loads your config.
-      * To be used from `main()` or equivalent.
-      */
+    /** Loads the configu. To be used from `main()` or equivalent. */
     def loadFromEnvironment(): ExoConfig =
-        load(ConfigFactory.load(System.getProperty(
-            "config.resource", "application.conf")))
+        load(ConfigFactory
+            .load(System.getProperty("config.resource", "application.conf"))
+            .withFallback(defaultConfig()))
 
     /** Load from a given Typesafe Config object */
     def load(config: Config): ExoConfig =
         ExoConfig(
             appConfig = AppConfig(),
             catalogConfig = CatalogConfig(
-                workerCount = Option(config.getInt("echo.catalog.worker-count")).getOrElse(5),
-                databaseUrl = Option(config.getString("echo.catalog.database-url")).getOrElse("jdbc:h2:mem:echo1"),
-                defaultPage = Option(config.getInt("echo.catalog.default-page")).getOrElse(1),
-                defaultSize = Option(config.getInt("echo.catalog.default-size")).getOrElse(20),
-                maxPageSize = Option(config.getInt("echo.catalog.max-page-size")).getOrElse(10000)
+                workerCount = config.getInt("echo.catalog.worker-count"),
+                databaseUrl = config.getString("echo.catalog.database-url"),
+                defaultPage = config.getInt("echo.catalog.default-page"),
+                defaultSize = config.getInt("echo.catalog.default-size"),
+                maxPageSize = config.getInt("echo.catalog.max-page-size")
             ),
             crawlerConfig = CrawlerConfig(
-                workerCount = Option(config.getInt("echo.crawler.worker-count")).getOrElse(5),
-                fetchWebsites = Option(config.getBoolean("echo.crawler.fetch-websites")).getOrElse(false),      // TODO rename to config file
-                downloadTimeout = Option(config.getInt("echo.crawler.download-timeout")).getOrElse(10),         // TODO add to config file
-                downloadMaxBytes = Option(config.getLong("echo.crawler.download-max-bytes")).getOrElse(5242880), // = 5  * 1024 * 1024 // TODO add to config file
+                workerCount      = config.getInt("echo.crawler.worker-count"),
+                fetchWebsites    = config.getBoolean("echo.crawler.fetch-websites"),  // TODO rename to config file
+                downloadTimeout  = config.getInt("echo.crawler.download-timeout"),    // TODO add to config file
+                downloadMaxBytes = config.getLong("echo.crawler.download-max-bytes"), // = 5  * 1024 * 1024 // TODO add to config file
             ),
             indexConfig = IndexConfig(
-                indexPath = Option(config.getString("echo.index.index-path")).getOrElse("/Users/max/volumes/echo/index_1"), // TODO add to config file
-                createIndex = Option(config.getBoolean("echo.index.create-index")).getOrElse(false),
-                commitInterval = Option(config.getInt("echo.index.commit-interval")).getOrElse(3).seconds,
-                workerCount = Option(config.getInt("echo.index.handler-count")).getOrElse(5)
+                indexPath      = config.getString("echo.index.index-path"), // TODO add to config file
+                createIndex    = config.getBoolean("echo.index.create-index"),
+                commitInterval = config.getInt("echo.index.commit-interval").seconds,
+                workerCount    = config.getInt("echo.index.handler-count")
             ),
             parserConfig = ParserConfig(
-                workerCount = Option(config.getInt("echo.parser.worker-count")).getOrElse(2)
+                workerCount = config.getInt("echo.parser.worker-count")
             ),
             updaterConfig = UpdaterConfig(),
-            internalTimeout = Option(config.getInt("echo.internal-timeout")).getOrElse(5).seconds
+            internalTimeout = config.getInt("echo.internal-timeout").seconds
         )
+
+    private def defaultConfig(): Config = {
+        val defaults = Map(
+            "echo.catalog.worker-count"       -> 5,
+            "echo.catalog.database-url"       -> "jdbc:h2:mem:echo1",
+            "echo.catalog.default-page"       -> 1,
+            "echo.catalog.default-size"       -> 20,
+            "echo.catalog.max-page-size"      -> 10000,
+            "echo.crawler.worker-count"       -> 5,
+            "echo.crawler.fetch-websites"     -> false, // TODO rename to config file
+            "echo.crawler.download-timeout"   -> 10, // TODO add to config file
+            "echo.crawler.download-max-bytes" -> 5242880, // = 5  * 1024 * 1024 // TODO add to config file
+            "echo.index.index-path"           -> "/Users/max/volumes/echo/index_1",
+            "echo.index.create-index"         -> false,
+            "echo.index.commit-interval"      -> 3,
+            "echo.index.handler-count"        -> 5,
+            "echo.parser.worker-count"        -> 2,
+            "echo.internal-timeout"           -> 5
+        )
+        ConfigFactory.parseMap(defaults.asJava)
+    }
 }
