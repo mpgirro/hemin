45b5eecc5931544481538ffa7011dd9ec022d2d9
Maximilian Irro <max@irro.at>
2018-10-25 09:57:55 +0200

    Remove some redundant logging output

--- hemin-api/app/api/v1/services/EngineService.scala
+++ hemin-api/app/api/v1/services/EngineService.scala
@@ -14,11 +14,9 @@ class EngineService @Inject() (lifecycle: ApplicationLifecycle) {
   private val log = Logger(getClass).logger
   private val config = ConfigFactory.load(System.getProperty("config.resource", "application.conf"))
 
-  log.info("Starting engine")
   val engine: Engine = new Engine(config)
 
   lifecycle.addStopHook { () =>
-    log.info("Shutting down engine")
     Future.successful(engine.shutdown()) // TODO sure that I want future.successful?
   }
 
