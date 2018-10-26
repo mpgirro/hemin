834e38a1bd5049f434ffc02eb42cefca3fa55012
Maximilian Irro <max@irro.at>
2018-10-27 01:11:58 +0200

    Use new Engine factory in EngineService

--- hemin-api/app/api/v1/services/EngineService.scala
+++ hemin-api/app/api/v1/services/EngineService.scala
@@ -1,20 +1,29 @@
 package api.v1.services
 
-import com.typesafe.config.ConfigFactory
+import com.typesafe.config.{Config, ConfigFactory}
 import io.hemin.engine.Engine
 import javax.inject._
 import play.api.Logger
 import play.api.inject.ApplicationLifecycle
 
 import scala.concurrent.Future
+import scala.util.{Failure, Success}
 
 @Singleton
 class EngineService @Inject() (lifecycle: ApplicationLifecycle) {
 
   private val log = Logger(getClass).logger
-  private val config = ConfigFactory.load(System.getProperty("config.resource", "application.conf"))
 
-  val engine: Engine = new Engine(config)
+  private val config: Config = ConfigFactory.load(System.getProperty("config.resource", "application.conf"))
+
+  val engine: Engine = Engine.of(config) match {
+    case Success(e)  => e
+    case Failure(ex) =>
+      log.error(s"Terminating due failed Engine initialization; reason : ${ex.getMessage}")
+      ex.printStackTrace()
+      System.exit(-1)
+      null // TODO can I return a better result value (just to please the compiler?)
+  }
 
   lifecycle.addStopHook { () =>
     Future.successful(engine.shutdown())
