66d0d900d1ef56b869db4d7e838ef8d6ed193dc6
Maximilian Irro <max@irro.at>
2018-10-31 20:09:40 +0100

    Migrate the internal standard exception from Java to Scala

--- hemin-engine/src/main/scala/io/hemin/engine/exception/HeminException.scala
+++ hemin-engine/src/main/scala/io/hemin/engine/exception/HeminException.scala
@@ -1,15 +1,15 @@
 package io.hemin.engine.exception
 
-object EngineException {
+object HeminException {
   /** Allows to access the message and the cause via pattern-matching */
-  def unapply(e: EngineException): Option[(String,Throwable)] = Some((e.getMessage, e.getCause))
+  def unapply(e: HeminException): Option[(String,Throwable)] = Some((e.getMessage, e.getCause))
 }
 
 /**
   *
   * @see The class design follows the recommendation in this post [[https://stackoverflow.com/a/43942163/9812594]]
   */
-class EngineException (message: String)
+class HeminException (message: String)
   extends Exception(message) {
 
   def this(message: String, cause: Throwable) = {
