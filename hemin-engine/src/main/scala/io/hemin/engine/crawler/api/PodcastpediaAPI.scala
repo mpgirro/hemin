44d8258535ee70dc2cc2b4d5a0a2ca3a65c001e3
Maximilian Irro <max@irro.at>
2018-11-05 20:21:58 +0100

    Added API skeletons

---
+++ hemin-engine/src/main/scala/io/hemin/engine/crawler/api/PodcastpediaAPI.scala
@@ -0,0 +1,9 @@
+package io.hemin.engine.crawler.api
+
+class PodcastpediaAPI extends ExternalDirectoryAPI {
+
+  override def baseUrl: String = throw new UnsupportedOperationException("PodcastpediaAPI.baseUrl not yet implemented")
+
+  override def getFeedUrls(count: Int): List[String] = throw new UnsupportedOperationException("PodcastpediaAPI.getFeedUrls not yet implemented")
+
+}
