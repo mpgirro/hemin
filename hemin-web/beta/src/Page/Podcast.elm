57ed06fe052608228785b9586e064f1d2a0f48bb
Maximilian Irro <max@irro.at>
2018-12-15 14:45:40 +0100

    [beta] improve the podcast page

--- hemin-web/beta/src/Page/Podcast.elm
+++ hemin-web/beta/src/Page/Podcast.elm
@@ -7,7 +7,7 @@ import Html.Attributes exposing (..)
 import Http
 import RestApi
 import Skeleton exposing (Page, viewHttpFailure)
-import Util exposing (maybeAsString, maybeAsText)
+import Util exposing (maybeAsString, maybeAsText, emptyHtml)
 
 
 
@@ -96,11 +96,71 @@ view model =
 viewPodcast : Podcast -> Html msg
 viewPodcast podcast =
     div [ class "col-sm-8", class "col-md-6", class "col-lg-6", class "p-2", class "mx-auto" ]
-        [ h1 [] [ maybeAsText podcast.title ]
-        , Skeleton.viewLink (maybeAsString podcast.link)
-        , p [] [ maybeAsText podcast.description ]
+        [ viewCoverImage podcast
+        , viewTitle podcast
+        , viewLink podcast
+        , viewDecription podcast
+        , viewCategories podcast
         ]
 
+
+viewCoverImage : Podcast -> Html msg
+viewCoverImage podcast =
+    case podcast.image of
+        Just image ->
+          div []
+            [ img [ src image, alt "cover image" ] [] ]
+
+        Nothing ->
+            emptyHtml
+
+viewTitle : Podcast -> Html msg
+viewTitle podcast =
+    case podcast.title of
+        Just title ->
+            h1 [ class "mt-4", class "mb-0" ] [ maybeAsText podcast.title ]
+
+        Nothing ->
+            emptyHtml
+
+viewLink : Podcast -> Html msg
+viewLink podcast =
+    case podcast.link of
+        Just link ->
+            a [ href link ] [ text link ]
+
+        Nothing ->
+            emptyHtml
+
+viewDecription : Podcast -> Html msg
+viewDecription podcast =
+    case podcast.itunes.summary of
+         Just summary ->
+            viewDescriptionParagraph summary
+         Nothing ->
+             case podcast.description of
+                 Just description ->
+                    viewDescriptionParagraph description
+                 Nothing ->
+                    emptyHtml
+
+viewDescriptionParagraph : String -> Html msg
+viewDescriptionParagraph description =
+    p [ class "mt-4" ] [ text description ]
+
+viewCategories : Podcast -> Html msg
+viewCategories podcast =
+    div [ class "mt-3" ]
+      <| List.map viewCategory podcast.itunes.categories
+
+
+viewCategory : String -> Html msg
+viewCategory category =
+    -- TODO add the OpenIconic stuff
+    span [ class "Label", class "Label--gray", class "p-1", class "mr-1" ]
+        [ span [ class "oi", class "oi-tag" ] []
+        , text category
+        ]
 
 
 -- HTTP
