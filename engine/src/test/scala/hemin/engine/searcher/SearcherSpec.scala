4b896cba50ab6e7b9366a58dba278d594bce55a2
Maximilian Irro <max@irro.at>
2019-02-24 19:34:51 +0100

    [engine] implement more test cases for Searcher, but they currently fail due to broken Solr setup

--- engine/src/test/scala/hemin/engine/searcher/SearcherSpec.scala
+++ engine/src/test/scala/hemin/engine/searcher/SearcherSpec.scala
@@ -1,11 +1,18 @@
 package hemin.engine.searcher
 
 import akka.actor.{ActorRef, ActorSystem}
-import akka.testkit.{ImplicitSender, TestKit}
+import akka.pattern.ask
+import akka.testkit.{ImplicitSender, TestActorRef, TestKit}
+import akka.util.Timeout
+import hemin.engine.model.SearchResult
 import hemin.engine.node.Node.{ActorRefSupervisor, ReportSearcherInitializationComplete}
+import hemin.engine.searcher.Searcher.{SearchRequest, SearchResults}
 import hemin.engine.{HeminConfig, HeminEngine}
-import org.scalatest.{FlatSpecLike, Matchers}
+import org.scalatest.{FlatSpecLike, Ignore, Matchers}
 
+import scala.util.{Failure, Success}
+
+@Ignore
 class SearcherSpec
   extends TestKit(ActorSystem(HeminEngine.name))
     with ImplicitSender
@@ -14,10 +21,70 @@ class SearcherSpec
 
   val engineConfig: HeminConfig = HeminConfig.defaultEngineConfig
 
+  implicit val timeout: Timeout = engineConfig.node.internalTimeout
+
+  val defaultQuery: String = "foo"
+  val defaultPageNumber: Option[Int] = Some(1)
+  val defaultPageSize: Option[Int] = Some(1)
+
+  def assertEmptySearchResult(result: SearchResult): Unit = {
+    result.currPage shouldBe 0
+    result.maxPage shouldBe 0
+    result.totalHits shouldBe 0
+    result.results shouldBe Nil
+  }
+
+  def defaultTestSearcher(): TestActorRef[Searcher] = {
+    val searcher: TestActorRef[Searcher] = TestActorRef(system.actorOf(Searcher.props(engineConfig.searcher)))
+    searcher ! ActorRefSupervisor(testActor)
+    expectMsgType[ReportSearcherInitializationComplete.type]
+    searcher
+  }
+
   "The Searcher" should "report its completed initialization" in {
     val searcher: ActorRef = system.actorOf(Searcher.props(engineConfig.searcher))
     searcher ! ActorRefSupervisor(testActor)
     expectMsgType[ReportSearcherInitializationComplete.type]
+  }
+
+  it should "reply with no search results found if the query is empty" in {
+    val searcher: ActorRef = defaultTestSearcher()
+    val future = searcher ? SearchRequest("", defaultPageNumber, defaultPageSize)
+    future.value.get match {
+      case Success(result: SearchResults) => assertEmptySearchResult(result.results)
+      case Success(other) => fail("Expected reply was not of type : " + classOf[SearchResults])
+      case Failure(ex) => throw ex
+    }
+  }
+
+  it should "reply with no search results found if the query is null" in {
+    val searcher: ActorRef = defaultTestSearcher()
+    val future = searcher ? SearchRequest(null, defaultPageNumber, defaultPageSize)
+    future.value.get match {
+      case Success(result: SearchResults) => assertEmptySearchResult(result.results)
+      case Success(other) => fail("Expected reply was not of type : " + classOf[SearchResults])
+      case Failure(ex) => throw ex
+    }
+  }
+
+  it should "reply with no search results found if the page number is invalid (<1)" in {
+    val searcher: ActorRef = defaultTestSearcher()
+    val future = searcher ? SearchRequest(defaultQuery, Some(0), defaultPageSize)
+    future.value.get match {
+      case Success(result: SearchResults) => assertEmptySearchResult(result.results)
+      case Success(other) => fail("Expected reply was not of type : " + classOf[SearchResults])
+      case Failure(ex) => throw ex
+    }
+  }
+
+  it should "reply with no search results found if the page size is invalid (<1)" in {
+    val searcher: ActorRef = defaultTestSearcher()
+    val future = searcher ? Searcher.SearchRequest(defaultQuery, defaultPageNumber, Some(0))
+    future.value.get match {
+      case Success(result: SearchResults) => assertEmptySearchResult(result.results)
+      case Success(other) => fail("Expected reply was not of type : " + classOf[SearchResults])
+      case Failure(ex) => throw ex
+    }
   }
 
 }
---
+++ engine/src/test/scala/hemin/engine/searcher/retriever/SolrRetrieverSpec.scala
@@ -0,0 +1,91 @@
+package hemin.engine.searcher.retriever
+
+import java.util.concurrent.Executors
+
+import hemin.engine.HeminConfig
+import hemin.engine.model.SearchResult
+import org.scalatest.{FlatSpec, Ignore, Matchers}
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
+
+@Ignore
+class SolrRetrieverSpec
+  extends FlatSpec
+    with Matchers {
+
+  implicit val executionContext: ExecutionContext = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(1))
+
+  val engineConfig: HeminConfig = HeminConfig.defaultEngineConfig
+
+  val defaultQuery: String = "foo"
+  val defaultPageNumber: Int = 1
+  val defaultPageSize: Int = 1
+
+  def assertEmptySearchResult(result: SearchResult): Unit = {
+    result.currPage shouldBe 0
+    result.maxPage shouldBe 0
+    result.totalHits shouldBe 0
+    result.results shouldBe Nil
+  }
+
+  "The SolrRetriever" should "succeed to initialize when connecting to the Solr mock server" in {
+    val retriever: IndexRetriever = new SolrRetriever(engineConfig.searcher, executionContext)
+    // TODO add an assertion?
+  }
+
+  it should "retrieve no search results if the query is empty" in {
+    val retriever: IndexRetriever = new SolrRetriever(engineConfig.searcher, executionContext)
+    val future: Future[SearchResult] = retriever.search(
+      query = "",
+      page = defaultPageNumber,
+      size = defaultPageSize
+    )
+    future.value.get match {
+      case Success(result) => assertEmptySearchResult(result)
+      case Failure(ex) => throw ex
+    }
+  }
+
+
+  it should "retrieve no search results if the query is null" in {
+    val retriever: IndexRetriever = new SolrRetriever(engineConfig.searcher, executionContext)
+    val future: Future[SearchResult] = retriever.search(
+      query = null,
+      page = defaultPageNumber,
+      size = defaultPageSize
+    )
+    future.value.get match {
+      case Success(result) => assertEmptySearchResult(result)
+      case Failure(ex) => throw ex
+    }
+  }
+
+  it should "retrieve no search results if the page number is invalid (<1)" in {
+    val retriever: IndexRetriever = new SolrRetriever(engineConfig.searcher, executionContext)
+    val future: Future[SearchResult] = retriever.search(
+      query = defaultQuery,
+      page = 0,
+      size = defaultPageSize
+    )
+    future.value.get match {
+      case Success(result) => assertEmptySearchResult(result)
+      case Failure(ex) => throw ex
+    }
+  }
+
+
+  it should "retrieve no search results if the page size is invalid (<1)" in {
+    val retriever: IndexRetriever = new SolrRetriever(engineConfig.searcher, executionContext)
+    val future: Future[SearchResult] = retriever.search(
+      query = defaultQuery,
+      page = defaultPageNumber,
+      size = 0
+    )
+    future.value.get match {
+      case Success(result) => assertEmptySearchResult(result)
+      case Failure(ex) => throw ex
+    }
+  }
+
+}
