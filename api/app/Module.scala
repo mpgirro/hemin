0f3935fb891e1439219aeaecc04c2eb66e6426ce
Maximilian Irro <max@irro.at>
2019-04-02 18:26:01 +0200

    [api] remove obsolete code

--- api/app/Module.scala
+++ api/app/Module.scala
@@ -1,9 +1,7 @@
-import javax.inject._
 import com.google.inject.AbstractModule
+import hemin.api.v1.service.EngineService
 import net.codingwell.scalaguice.ScalaModule
 import play.api.{Configuration, Environment}
-import example.post._
-import hemin.api.v1.service.EngineService
 
 /**
   * Sets up custom components for Play.
@@ -14,10 +12,6 @@ class Module(environment: Environment, configuration: Configuration)
     extends AbstractModule with ScalaModule {
 
     override def configure(): Unit = {
-        bind[PostRepository]
-            .to[PostRepositoryImpl]
-            .in[Singleton]
-
         /*
          * This is important!
          * We make an eager binding of the EngineService.
